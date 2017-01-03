0137c79799b861dbb432ba9795535824df50bbbd
Attonasi <attonasi@hotmail.com>
2017-01-03 14:17:28 -0800

    done todos

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,18 +1,29 @@
 var articles = [];
 
 function Article (options) {
-/* TODO: This is our Model constructor. It will take in
+/* DONE: This is our Model constructor. It will take in
    our source data from blogArticles and instantiate a
    new Object according to this new definition. options is
    a placeholder for the object that will ultimately be
    passed in. Use all of the properties in blogArticles
    to populate the new Article data that we'll use.  */
-   this.title = options.title;
+  this.title = options.title;
+  this.category = options.category;
+  this.author = options.author;
+  this.authorUrl = options.authorUrl;
+  this.publishedOn = options.publishedOn;
+  this.body = options.body;
+
 };
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
+  $newArticle.find('h1').text(this.title);
+  // $newArticle.find('address').text(this.author);
+  $newArticle.find('a').text(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('section.article-body').html(this.body);
   /* TODO: We also need to fill in:
   1. author name
   2. author url
